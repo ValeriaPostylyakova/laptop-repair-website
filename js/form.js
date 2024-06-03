a2e09e90da8f52a94e484e14ad11121e32918439
valeriapostylyakova <postylyakova.valeria@mail.ru>
2024-06-03 21:44:31 +0300

    sectionForm3

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
-const element = document.getElementById('phone');
-      const maskOptions = {
-       mask: '+{7}(000)000-00-00'
-      };
-      const mask = IMask(element, maskOptions);
+const phone = document.querySelectorAll('input[name="phone"]');
+const maskOptions = {
+	mask: '+7(000)000-00-00',
+	lazy: true
+};
+for (var i = 0; i < phone.length; i++){
+	var mask = new IMask(phone[i], maskOptions);
+}
